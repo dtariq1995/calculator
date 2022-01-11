9e382c0df38c1b1f1609173ebc07d690bccb04bf
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2022-01-10 20:31:06 -0600

    Added totally original styling to the calculator and got display update working. Still working on functionality of calculator

--- calculator.js
+++ calculator.js
@@ -31,6 +31,42 @@ function operate(operator, num1, num2) {
     }
 }
 
+function updateDisplay() {
+    let display = document.querySelector("#display");
+    display.textContent = "45";
+}
+
+
+
+function getOperator() {
+
+    let operatorButtons = document.querySelectorAll('.operator');
+    operatorButtons.forEach((operator) => {
+        operator.addEventListener('click', () => {
+            operatorSelection = operator.textContent;
+            console.log(operatorSelection);
+            return operatorSelection;
+        });
+    });
+}
+
+
+
+function getNums() {
+
+    let display = document.querySelector('#display');
+    let num1 = "";
+    let digitButtons = document.querySelectorAll('.digit');
+    digitButtons.forEach((digits) => {
+        digits.addEventListener('click', () => {
+            num1 = num1.concat(digits.textContent);
+            display.textContent = num1;
+            console.log(num1);
+            return num1;
+        });
+    });
+}
+
 
 a = 15;
 b = 3;
@@ -38,3 +74,12 @@ console.log(add(a, b));
 console.log(subtract(a, b));
 console.log(multiply(a, b));
 console.log(divide(a, b));
+console.log(operate("+", a, b));
+
+let display = document.querySelector('#display');
+
+
+getNums();
+getOperator();
+
+
