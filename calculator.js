2045cdc363813368035dbb53cdc59b31e003a077
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2022-01-13 19:50:34 -0600

    Added functionality to AC button and refactored some code '
    '
    
    ''

--- calculator.js
+++ calculator.js
@@ -54,18 +54,47 @@ function getOperator() {
 
 function getNums() {
 
-    let display = document.querySelector('#display');
-    let num1 = "";
+
     let digitButtons = document.querySelectorAll('.digit');
     digitButtons.forEach((digits) => {
         digits.addEventListener('click', () => {
             num1 = num1.concat(digits.textContent);
             display.textContent = num1;
             console.log(num1);
-            return num1;
+        });
+    });
+
+
+    let operatorButtons = document.querySelectorAll('.operator');
+    operatorButtons.forEach((operator) => {
+        operator.addEventListener('click', () => {
+            operatorSelection = operator.textContent;
+            console.log(operatorSelection);
+            
+            let digitsButtons = document.querySelectorAll('.digit');
+            digitsButtons.forEach((digit) => {
+                digit.addEventListener('click', () => {
+                    num2 = num2.concat(digit.textContent);
+                    display.textContent = num2;
+                    console.log(num2);
+                });
+            });
         });
     });
 }
+
+
+function clear() {
+
+    let clear = document.querySelector('#clear');
+    clear.addEventListener('click', () => {
+        display.textContent = 0;
+        num1 = "";
+        operatorSelection = "";
+        num2 = "";
+    });
+}
+
 
 
 a = 15;
@@ -77,9 +106,13 @@ console.log(divide(a, b));
 console.log(operate("+", a, b));
 
 let display = document.querySelector('#display');
+let num1 = "";
+let operatorSelection= "";
+let num2 = "";
 
 
 getNums();
-getOperator();
+clear();
+
 
 
